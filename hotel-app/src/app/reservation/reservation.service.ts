a906f5c77464a2f35d579d999f5f1f90cb5bc5cc
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-04-23 12:00:41 +0200

    Update service with CRUD actions

--- hotel-app/src/app/reservation/reservation.service.ts
+++ hotel-app/src/app/reservation/reservation.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = []
+
+  getReservations(): Reservation[] {
+    return this.reservations
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find(reservation => reservation.id === id)
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation)
+  }
+
+  deleteReservation(id: string): void {
+    const index = this.reservations.findIndex(reservation => reservation.id === id)
+    this.reservations.splice(index, 1)
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    const index = this.reservations.findIndex(reservation => reservation.id === updatedReservation.id)
+    this.reservations[index] = updatedReservation
+  }
 }
