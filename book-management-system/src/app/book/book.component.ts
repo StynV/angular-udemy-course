2cd36369e4ff2d3c6c41e899facd2183e85a5ef3
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-04-23 09:16:41 +0200

    Save in local storage

--- book-management-system/src/app/book/book.component.ts
+++ book-management-system/src/app/book/book.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Book } from '../models/book.model'
 
@@ -10,11 +10,21 @@ import { Book } from '../models/book.model'
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
-export class BookComponent {
+
+export class AppointmentListComponent implements OnInit {
   newBookTitle: string = ""
   newBookAuthor: string = ""
 
   books: Book[] = []
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const savedBooks = localStorage.getItem("books")
+      this.books = savedBooks ? JSON.parse(savedBooks) : []
+    }
+  }
 
   addBook() {
     if (this.newBookTitle.trim().length && this.newBookAuthor.trim().length) {
@@ -28,6 +38,13 @@ export class BookComponent {
 
       this.newBookTitle = ""
       this.newBookAuthor = ""
+
+      localStorage.setItem("books", JSON.stringify(this.books))
     }
+  }
+
+  deleteBook(index: number) {
+    this.books.splice(index, 1)
+    localStorage.setItem("books", JSON.stringify(this.books))
   }
 }
