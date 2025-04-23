4d0591a71a81a414924ea154d38b4c99ff485332
Styn Vercauteren <styn.vercauteren@outlook.com>
2025-04-23 11:06:04 +0200

    Start on form

--- hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,11 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
-  imports: [],
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.scss'
+  styleUrl: './reservation-form.component.scss',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
 })
-export class ReservationFormComponent {
 
+export class ReservationFormComponent {
+  reservationForm: FormGroup = new FormGroup({})
 }
--- hotel-app/src/app/reservation/reservation.module.ts
+++ hotel-app/src/app/reservation/reservation.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ReservationFormComponent,
+    ReservationListComponent,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
     ReservationFormComponent,
     ReservationListComponent
   ]
